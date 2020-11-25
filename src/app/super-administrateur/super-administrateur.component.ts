3f0a54ce69378dd464be131ea8b07b4b08a38e6d
marwensghair95 <marwensghair95@gmail.com>
2020-11-25 22:50:02 +0100

    create sup admin

---
+++ src/app/super-administrateur/super-administrateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-super-administrateur',
+  templateUrl: './super-administrateur.component.html',
+  styleUrls: ['./super-administrateur.component.css']
+})
+export class SuperAdministrateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
