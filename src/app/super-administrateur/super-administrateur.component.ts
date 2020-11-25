847e335634ae28a084093f17e754f48ec3a6a34e
marwensghair95 <marwensghair95@gmail.com>
2020-11-26 00:52:34 +0100

    super admin

--- src/app/super-administrateur/super-administrateur.component.ts
+++ src/app/super-administrateur/super-administrateur.component.ts
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./super-administrateur.component.css']
 })
 export class SuperAdministrateurComponent implements OnInit {
+  users = [
+    {
+     
+      etbName: "alpha formation",
+      email: "marwen@gmail.com",
+      fax: "7285293",
+      fname: "hhhh",
+      lname: "hhhhhhhhhh",
+      password: "hhhhhhhhhhhhhhh",
+      confpassword: "hhhhhhhh",
+      phone: "85963741",
+      typeUser: "candidate",
+
+    },
+    {
+      etbName: "fivePoints",
+      email: "marwen@gmail.com",
+      fax: "7812346",
+      fname: "hhhh",
+      lname: "hhhhhhhhhh",
+      password: "hhhhhhhhhhhhhhh",
+      confpassword: "hhhhhhhh",
+      phone: "7418523",
+      typeUser: "candidate",
+
+    }, {
+      etbName: "skpaa",
+      email: "marwen@gmail.com",
+      fax: "4567892",
+      fname: "hhhh",
+      lname: "hhhhhhhhhh",
+      password: "hhhhhhhhhhhhhhh",
+      confpassword: "hhhhhhhh",
+      phone: "85963741",
+      typeUser: "candidate",
+
+    }
+  ];
 
   constructor() { }
 
