34e3622b0e0d108455ee5a6945f1e3dddf5b64c4
emna-hamdi <emna.hamdi.adressemail@gmail.com>
2020-12-01 11:48:28 +0100

    merge emna

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,10 @@ import { LoginComponent } from './login/login.component';
 import { Error404Component } from './error404/error404.component';
 import { Error500Component } from './error500/error500.component';
 import { ProfilComponent } from './profil/profil.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatCardModule} from '@angular/material/card';
-import {MatRippleModule} from '@angular/material/core';
-import {CdkTableModule} from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { CdkTableModule } from '@angular/cdk/table';
 import { AvatarComponent } from './avatar/avatar.component';
 import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 import { SuperAdministrateurComponent } from './super-administrateur/super-administrateur.component';
@@ -50,11 +50,11 @@ import { MyApplicationModule } from './my-application/my-application.module';
     MatTableModule,
     MatCardModule,
     MatRippleModule,
-     CdkTableModule,
-     RxReactiveFormsModule,
-     AlifeFileToBase64Module,
-     MyApplicationModule,
-     RxReactiveFormsModule
+    CdkTableModule,
+    RxReactiveFormsModule,
+    AlifeFileToBase64Module,
+    MyApplicationModule,
+    RxReactiveFormsModule
 
   ],
   providers: [],
